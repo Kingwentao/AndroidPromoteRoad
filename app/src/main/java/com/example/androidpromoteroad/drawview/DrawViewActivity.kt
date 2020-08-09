ffb5cb877d494bb8c5cd5f8c5806fc4ff3ea75b6
wentaoKing <jinwentao@cvte.com>
2020-08-09 16:01:59 +0800

    start learnandroid of draw view

---
+++ app/src/main/java/com/example/androidpromoteroad/drawview/DrawViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidpromoteroad.drawview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidpromoteroad.R
+
+class DrawViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_draw_view)
+    }
+}
--- app/src/main/java/com/example/androidpromoteroad/drawview/FirstView.kt
+++ app/src/main/java/com/example/androidpromoteroad/drawview/FirstView.kt
@@ -2,18 +2,36 @@ package com.example.androidpromoteroad.drawview
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
+import com.example.androidpromoteroad.utils.dp2px
 
 /**
  * author: created by wentaoKing
  * date: created in 2020/8/5
- * description:
+ * description: 自定义view
  */
-class FirstView(context: Context,attrs: AttributeSet?): View(context,attrs) {
+class FirstView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val path = Path()
+    val radius = 100f.dp2px
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+        canvas?.drawPath(path,paint)
+    }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        path.reset()
+        path.addCircle(width / 2f, height / 2f, radius, Path.Direction.CCW)
+        path.addRect(
+            width / 2f - radius, height / 2f - radius, width / 2f + radius,
+            height / 2f + radius, Path.Direction.CCW
+        )
+        //EVEN_ODD 相交区域直接镂空，不区分绘制图行的方向
+        //WINDING 会根据绘制方向判断填充和镂空
+        path.fillType = Path.FillType.EVEN_ODD
     }
 }
