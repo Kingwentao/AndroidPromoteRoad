2011405fbc8b5525af2b5e8ff412990a53703db1
jinwentao <jinwentao@cvte.com>
2020-08-29 18:22:11 +0800

    add the annotation process learn

---
+++ app/src/main/java/com/example/androidpromoteroad/annotationprocess/InnerBinding.java
@@ -0,0 +1,12 @@
+package com.example.androidpromoteroad.annotationprocess;
+
+/**
+ * author: WentaoKing
+ * created on: 2020/8/29
+ * description:
+ */
+class InnerBinding {
+    void bind(){
+
+    }
+}
---
+++ lib-reflection/src/main/java/com/example/lib_reflection/BindView.java
@@ -0,0 +1,15 @@
+package com.example.lib_reflection;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * author: WentaoKing
+ * created on: 2020/8/29
+ * description:
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@
+public @interface BindView {
+}
