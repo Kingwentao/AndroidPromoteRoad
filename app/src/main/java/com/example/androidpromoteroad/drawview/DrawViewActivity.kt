01e899662b356b559feba632dccac5d74c15c1f1
wentaoKing <jinwentao@cvte.com>
2020-09-04 06:55:33 +0800

    添加了文字的绘制

--- app/src/main/java/com/example/androidpromoteroad/drawview/DrawViewActivity.kt
+++ app/src/main/java/com/example/androidpromoteroad/drawview/DrawViewActivity.kt
@@ -3,7 +3,6 @@ package com.example.androidpromoteroad.drawview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidpromoteroad.R
-import kotlinx.android.synthetic.main.activity_draw_view.*
 
 class DrawViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/androidpromoteroad/drawview/textmeasure/SportView.kt
+++ app/src/main/java/com/example/androidpromoteroad/drawview/textmeasure/SportView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
@@ -21,6 +22,7 @@ private val HIGHLIGHT_COLOR = Color.parseColor("#FF4081")
 private val RING_WIDTH = 20.dp2px
 private val RADIUS = 150.dp2px
 
+
 class SportView(context: Context, attrs: AttributeSet?) :
     View(context, attrs) {
 
@@ -31,6 +33,8 @@ class SportView(context: Context, attrs: AttributeSet?) :
         //isFakeBoldText 假的粗体，它是描出来的
         //isFakeBoldText = true
     }
+    private val fontMetrics = Paint.FontMetrics()
+    private val bounds = Rect()
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -44,10 +48,17 @@ class SportView(context: Context, attrs: AttributeSet?) :
         // 绘制进度条
         paint.color = HIGHLIGHT_COLOR
         paint.strokeCap = Paint.Cap.ROUND
-        canvas.drawArc(width / 2f - RADIUS, height / 2f - RADIUS,
-            width / 2f + RADIUS, height / 2f + RADIUS, -90f, 225f, false, paint)
+        canvas.drawArc(
+            width / 2f - RADIUS, height / 2f - RADIUS,
+            width / 2f + RADIUS, height / 2f + RADIUS,
+            -90f, 225f, false, paint
+        )
+
+        //绘制文字
+        paint.getFontMetrics(fontMetrics)
+        paint.getTextBounds("abab", 0, "abab".length, bounds)
+        canvas.drawText("abab", width /2f ,height/2f - (bounds.top + bounds.bottom)/2f, paint)
+
     }
-
-
 
 }
