f5551522122325c2bba2c582766562f580386369
wentaoKing <jinwentao@cvte.com>
2020-09-08 07:00:46 +0800

    start generic learn

---
+++ app/src/main/java/com/example/androidpromoteroad/generic/Fruit.java
@@ -0,0 +1,9 @@
+package com.example.androidpromoteroad.generic;
+
+/**
+ * author: created by wentaoKing
+ * date: created in 2020/9/8
+ * description: 水果接口
+ */
+interface Fruit {
+}
---
+++ app/src/main/java/com/example/androidpromoteroad/generic/shop/Shop.java
@@ -0,0 +1,12 @@
+package com.example.androidpromoteroad.generic.shop;
+
+/**
+ * author: created by wentaoKing
+ * date: created in 2020/9/8
+ * description:
+ */
+interface Shop<T> {
+
+    T getFruit();
+
+}
