7be12a1fe06bfc855579f6eef2694593a656a7d0
jinwentao <jinwentao@cvte.com>
2020-09-11 13:15:34 +0800

    add the generice of extends,super

--- app/src/main/java/com/example/androidpromoteroad/generic/shop/MainFruit.java
+++ app/src/main/java/com/example/androidpromoteroad/generic/shop/MainFruit.java
@@ -14,22 +14,28 @@ import java.util.ArrayList;
 class MainFruit {
     public static void main(String[] args) {
         //1. 错误写法
-        ArrayList<Fruit> fruits = new ArrayList<Apple>();
-        fruits.add(new Orange()); //添加橘子到苹果集合，为啥可以不报错？
+//        ArrayList<Fruit> fruits = new ArrayList<Apple>();
+//        fruits.add(new Orange()); //添加橘子到苹果集合，为啥可以不报错？
 
         //2. 正确写法1
         ArrayList<Fruit> fruits2 = new ArrayList<Fruit>();
-        fruits.add(new Orange());
-        fruits.add(new Apple());
+        fruits2.add(new Orange());
+        fruits2.add(new Apple());
 
         //3. 或者这么写
         ArrayList<? extends Fruit> fruits3 = new ArrayList<Apple>();
+        //3.1 不能添加
         fruits3.add(new Orange());  //不可以添加橘子
         fruits3.add(new Apple());  //为啥不可以添加苹果？？？
-
-        Apple apple = (Apple) fruits3.get(0);
+        //3.2 只能获取
+        Apple apple = (Apple) fruits3.get(1);
         Orange orange = (Orange) fruits3.get(0);
 
+        ArrayList<? super Apple> apples = new ArrayList<>();
+        Fruit fruit = new Fruit() {
 
+        };
+        apples.add(fruit);
+        Apple apple2 = apples.get(0);
     }
 }
